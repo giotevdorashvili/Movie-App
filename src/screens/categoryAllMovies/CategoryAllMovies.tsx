8329edc78cabd2b185623f79d203679cc412a721
giotevdorashvili <giorgi.tevdorashvili@makingscience.com>
2023-12-01 17:38:08 +0400

    Add: scroll to top functionality on CategoryAllMovies screen

--- src/screens/categoryAllMovies/CategoryAllMovies.tsx
+++ src/screens/categoryAllMovies/CategoryAllMovies.tsx
@@ -1,18 +1,31 @@
-import React, {useEffect, useMemo} from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useEffect, useMemo, useState, useRef} from 'react';
+import {
+  StyleSheet,
+  useWindowDimensions,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {ActivityIndicator, Text} from 'react-native-paper';
+import {ActivityIndicator, Text, AnimatedFAB} from 'react-native-paper';
 import {FlashList} from '@shopify/flash-list';
 
 import useCategeryAllMovies from '../../hooks/services/useCategeryAllMovies';
 import {ScreenProps} from '../../navigators/StackNavigator';
 import useRenderItem from '../../hooks/flatLIst/useRenderItem';
+import {PaperTheme} from '../../theme/theme';
+
+type OnScrollEventHandler = (
+  event: NativeSyntheticEvent<NativeScrollEvent>,
+) => void;
 
 const CategoryAllMovies: React.FC<ScreenProps<'CategoryAllMovies'>> = ({
   route,
   navigation,
 }) => {
   const {keyName, title} = route.params;
+  const {height} = useWindowDimensions();
+  const [visible, setVisible] = useState(false);
+  const flashListRef = useRef<FlashList<any>>(null);
 
   const {
     data,
@@ -41,6 +54,18 @@ const CategoryAllMovies: React.FC<ScreenProps<'CategoryAllMovies'>> = ({
     }
   };
 
+  const handleScroll: OnScrollEventHandler = event => {
+    if (event.nativeEvent.contentOffset.y > height * 0.8) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
+
+  const handleFabPress = () => {
+    flashListRef.current?.scrollToIndex({animated: true, index: 0});
+  };
+
   if (isLoading) {
     return <ActivityIndicator style={styles.container} />;
   }
@@ -52,13 +77,26 @@ const CategoryAllMovies: React.FC<ScreenProps<'CategoryAllMovies'>> = ({
   return (
     <SafeAreaView style={styles.container}>
       <FlashList
+        ref={flashListRef}
         data={dataResults}
         renderItem={renderItem}
         estimatedItemSize={20 * (data?.pages?.length || 1)}
         numColumns={3}
         onEndReached={handleIncreasePage}
         onEndReachedThreshold={0.1}
+        onScroll={handleScroll}
         keyExtractor={(movie, index) => `${movie.id.toString()}-${index}`}
+      />
+
+      <AnimatedFAB
+        icon="arrow-up-thin"
+        label={'Label'}
+        extended={false}
+        onPress={handleFabPress}
+        visible={visible}
+        animateFrom={'right'}
+        iconMode={'static'}
+        style={[styles.fab]}
       />
     </SafeAreaView>
   );
@@ -81,6 +119,12 @@ const styles = StyleSheet.create({
     height: 180,
     marginBottom: 10,
     marginHorizontal: 3,
+  },
+  fab: {
+    position: 'absolute',
+    bottom: '20%',
+    right: '10%',
+    backgroundColor: PaperTheme.colors.orange,
   },
 });
 
